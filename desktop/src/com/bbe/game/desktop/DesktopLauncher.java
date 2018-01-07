f000219815c18e1c1b39bb527bbb8a6ed1b60af7
erikl <erilundin@gmail.com>
2018-01-07 19:17:17 +0100

    dev progress

--- desktop/src/com/bbe/game/desktop/DesktopLauncher.java
+++ desktop/src/com/bbe/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.bbe.game.desktop;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.bbe.game.Wolfenstein;
@@ -8,6 +9,20 @@ import com.bbe.game.utils.Logger;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = 1280;
+		config.height = 720;
+		config.fullscreen = false;
+		config.resizable = false;
+		config.useGL30 = false;
+		config.title = "Wolfenstein";
+		config.vSyncEnabled = true;
+		config.forceExit = true;
+		if (config.fullscreen) {
+			Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+			config.setFromDisplayMode(displayMode);
+		}
+		// Window icons: 128x128(Mac), 32x32 (for Win and Linux), and 16x16 (for Win).
 		new LwjglApplication(new Wolfenstein(), config);
 
 		info();
@@ -17,6 +32,13 @@ public class DesktopLauncher {
 		Logger.log("OS name: " + System.getProperty("os.name"));
 		Logger.log("OS version: " + System.getProperty("os.version"));
 		Logger.log("Gdx version: " + com.badlogic.gdx.Version.VERSION);
+		Graphics.DisplayMode[] modes = LwjglApplicationConfiguration.getDisplayModes();
+		Logger.log("======================Graphic Modes=========================");
+		for (Graphics.DisplayMode mode: modes) {
+			Logger.log("" + mode);
+		}
+		Logger.log("============================================================");
+
 
 	}
 }
